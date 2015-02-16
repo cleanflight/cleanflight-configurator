203f95d3acebb3fa356b5b9b5314845c401373fd
tricopterY <tricoptery@gmail.com>
2015-02-16 20:36:38 +1100

    Update gui.js
    I had there before, good catch. Thanks.

--- js/gui.js
+++ js/gui.js
@@ -207,7 +207,7 @@ GUI_control.prototype.log = function (message) {
     var d = new Date();
     var year = d.getFullYear();
     var month = ((d.getMonth() < 9) ? '0' + (d.getMonth() + 1) : (d.getMonth() + 1));
-    var date =  d.getDate();
+    var date =  ((d.getDate() < 10) ? '0' + d.getDate() : d.getDate());
     var time = ((d.getHours() < 10) ? '0' + d.getHours(): d.getHours())
         + ':' + ((d.getMinutes() < 10) ? '0' + d.getMinutes(): d.getMinutes())
         + ':' + ((d.getSeconds() < 10) ? '0' + d.getSeconds(): d.getSeconds());

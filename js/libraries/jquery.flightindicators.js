1bc14af17beaa6771b46f8055e6730ee68c1e6d4
tricopterY <tricoptery@gmail.com>
2015-03-01 11:40:15 +1100

    Update jquery.flightindicators.js

--- js/libraries/jquery.flightindicators.js
+++ js/libraries/jquery.flightindicators.js
@@ -65,6 +65,7 @@
 
 		// Private methods
 		function _setRoll(roll){
+			roll = roll > 0 ? roll*(-1) : roll*(-1);
 			placeholder.each(function(){
 				$(this).find('div.instrument.attitude div.roll').css('transform', 'rotate('+roll+'deg)');
 			});

ccacad3946b76e72b4afa9a6712c22664b0c84c3
DrTriGG3R <ds.derks97@gmail.com>
2015-08-14 22:04:22 +0200

    Added configurator support for ddb's larson scanner

--- tabs/led_strip.js
+++ tabs/led_strip.js
@@ -2,7 +2,7 @@
 
 TABS.led_strip = {
     wireMode: false,
-    functions: ['w', 'f', 'i', 'a', 't', 'r', 'c'],
+    functions: ['w', 'f', 'i', 'a', 't', 'r', 'c', 'l'],
     directions: ['n', 'e', 's', 'w', 'u', 'd'],
 };
 

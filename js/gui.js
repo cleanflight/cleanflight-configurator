4680d4d946131d7eb5b8d3eec23017ff3cbc6017
David Zumbrunnen <zumbrunnen@gmail.com>
2016-03-06 17:52:19 +0100

    Simplified/localized timestamp for log

--- js/gui.js
+++ js/gui.js
@@ -208,19 +208,11 @@ GUI_control.prototype.timeout_kill_all = function () {
 GUI_control.prototype.log = function (message) {
     var command_log = $('div#log');
     var d = new Date();
-    var year = d.getFullYear();
-    var month = ((d.getMonth() < 9) ? '0' + (d.getMonth() + 1) : (d.getMonth() + 1));
-    var date =  ((d.getDate() < 10) ? '0' + d.getDate() : d.getDate());
-    var time = ((d.getHours() < 10) ? '0' + d.getHours(): d.getHours())
-         + ':' + ((d.getMinutes() < 10) ? '0' + d.getMinutes(): d.getMinutes())
-         + ':' + ((d.getSeconds() < 10) ? '0' + d.getSeconds(): d.getSeconds());
+    var date = d.toISOString().substring(0, 10);
+    var time = d.toLocaleTimeString();
 
-    var formattedDate = "{0}-{1}-{2} {3}".format(
-                                year,
-                                month,
-                                date,
-                                ' @ ' + time
-                            );
+    var formattedDate = [date, time].join(' @ ');
+
     $('div.wrapper', command_log).append('<p>' + formattedDate + ' -- ' + message + '</p>');
     command_log.scrollTop($('div.wrapper', command_log).height());
 };

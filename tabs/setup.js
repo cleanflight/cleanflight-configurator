9e2a9525481d126b8ecc5d60b0f174ebb0374b2b
gael <gaeljames@gmail.com>
2016-04-19 00:06:28 +0200

    Setup tab: Home direction as red dot on compass

--- tabs/setup.js
+++ tabs/setup.js
@@ -187,6 +187,9 @@ TABS.setup.initialize = function (callback) {
                 heading_e.text(chrome.i18n.getMessage('initialSetupAttitude', [SENSOR_DATA.kinematics[2]]));
                 self.render3D();
                 self.updateInstruments();
+                MSP.send_message(MSP_codes.MSP_COMP_GPS, false, false, function () {
+                    // GPS_DATA.directionToHome
+                } );
             });
         }
 
@@ -205,7 +208,7 @@ TABS.setup.initializeInstruments = function() {
     this.updateInstruments = function() {
         attitude.setRoll(SENSOR_DATA.kinematics[0]);
         attitude.setPitch(SENSOR_DATA.kinematics[1]);
-        heading.setHeading(SENSOR_DATA.kinematics[2]);
+        heading.setHeading(SENSOR_DATA.kinematics[2], GPS_DATA.directionToHome);
     };
 };
 

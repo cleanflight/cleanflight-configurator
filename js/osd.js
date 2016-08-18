b72abf3c0d27519c0ab98189e2ea1c2bcfe839e5
Hydra <dominic.clifton@cleanflight.com>
2016-08-18 17:19:45 +0200

    Missing file from previous commit.

---
+++ js/osd.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// define all the global variables that are uses to hold FC state
+var OSD_VIDEO_CONFIG;
+
+var OSD = {
+    resetState: function() {
+        OSD_VIDEO_CONFIG = {
+            video_mode: 0,
+        };        
+    }
+};

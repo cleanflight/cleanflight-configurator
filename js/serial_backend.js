c2169ed471db162d35d604ef40550ecdc219319a
Hydra <dominic.clifton@cleanflight.com>
2016-08-18 18:01:39 +0200

    Only enable Gyro status indicator for FCs.

--- js/serial_backend.js
+++ js/serial_backend.js
@@ -341,7 +341,7 @@ function sensor_status(sensors_detected) {
         $('.accicon', e_sensor_status).removeClass('active');
     }
 
-    if (true) { // Gyro status is not reported by FC
+    if (CONFIG.boardType == 0) { // Gyro status is not reported by FC 
         $('.gyro', e_sensor_status).addClass('on');
         $('.gyroicon', e_sensor_status).addClass('active');
     } else {

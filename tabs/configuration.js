265f0163eb121603cc9f962e6d0970479e474971
Sean Kelly <theseankelly@outlook.com>
2017-04-23 07:49:36 -0700

    BF/CF - Adding TARGET_CUSTOM SerialRx protocol option Allows configurator to choose to use custom target-defined serialRx protools. Requires taret support.

--- tabs/configuration.js
+++ tabs/configuration.js
@@ -474,6 +474,10 @@ TABS.configuration.initialize = function (callback, scrollPosition) {
             serialRXtypes.push('SRXL');
         }
 
+        if (semver.gte(CONFIG.apiVersion, "1.35.0")) {
+            serialRXtypes.push('TARGET_CUSTOM');
+        }
+
         var serialRX_e = $('select.serialRX');
         for (var i = 0; i < serialRXtypes.length; i++) {
             serialRX_e.append('<option value="' + i + '">' + serialRXtypes[i] + '</option>');
